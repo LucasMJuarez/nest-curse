3cf6246f95c03c168c159a7b4492f83fd8851672
lucjuare <mauro.avendano@everis.com>
2023-07-31 16:56:46 -0300

    update car part 1

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -13,6 +13,7 @@ import {
 } from '@nestjs/common';
 import { CarsService } from './cars.service';
 import { CreateCarDto } from './dto/create-car.dto';
+import { UpdateCarDto } from './dto/update-car.dto';
 
 @Controller('cars')
 export class CarsController {
@@ -36,8 +37,11 @@ export class CarsController {
   }
 
   @Patch(':id')
-  updateCar(@Body() body: any) {
-    return body;
+  updateCar(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() updateCarDto: UpdateCarDto,
+  ) {
+    return updateCarDto;
   }
 
   @Delete(':id')
